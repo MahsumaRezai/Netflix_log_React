264699cfed4be882783d46facd5e7faba6f00edc
Masoumeh Rezai <you@example.com>
2023-07-21 09:56:24 +0430

    add card page

---
+++ src/Componets/Card/Card.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Card = (props) => {
+    return (
+        <Fragment>
+            <div>
+                {props.childern}
+            </div>
+        </Fragment>
+    )
+}
+export default Card;
