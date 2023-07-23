a51ad5bbbb684c4e9442741b528d6d25366c1fd2
Masoumeh Rezai <you@example.com>
2023-07-23 11:24:58 +0430

    last commit for today

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import { Fragment } from 'react';
 import Adduser from './Componets/Adduser/Adduser';
 import Footer from './Componets/Footer/Footer';
+import Head from './Componets/Head/Head'
 function App() {
   return (
     <Fragment>
+      <Head/>
       <Adduser />
       <Footer/>
 
